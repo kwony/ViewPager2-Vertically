aa5c43149d88dfaad7f16d85ea216f84507760aa
Hyeonkwon Cho <kwony.cho@snowcorp.com>
2019-12-18 09:45:39 +0900

    refactoring

--- app/src/main/java/com/example/viewpager/MainActivity.kt
+++ app/src/main/java/com/example/viewpager/MainActivity.kt
@@ -6,14 +6,9 @@ import androidx.viewpager2.widget.ViewPager2
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    val arrayList = ArrayList<String>()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        arrayList.add("hello")
-        arrayList.add("world")
 
         val adapter = ViewPager2Adapter(this)
 
@@ -21,7 +16,6 @@ class MainActivity : AppCompatActivity() {
         adapter.addFragment(MyFragment.newInstance(1))
         adapter.addFragment(MyFragment.newInstance(2))
 
-        viewPager2.beginFakeDrag()
         viewPager2.adapter = adapter
         viewPager2.orientation = ViewPager2.ORIENTATION_VERTICAL
     }
